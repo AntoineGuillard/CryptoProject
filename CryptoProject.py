c0b385ddb2f33d16c1acca0646376590dcf44ea9
AntoineGuillard <antoine.leggio@gmail.com>
2020-02-26 20:51:01 +0100

    improvements to the project but does not decipher correctly

--- CryptoProject.py
+++ CryptoProject.py
@@ -3,97 +3,147 @@ from Crypto import Random
 from Crypto_main import *
 
 
-
-key = "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF"
-
-def cipherFile(key,inputFile):
-    #Generate IV
-    iv = Random.new().read(AES.block_size)
-    #Open file in reading binary mode and store it inside a variable
-    with open(inputFile, 'rb') as fileToCipher:
-        newByteFile = bytearray(fileToCipher.read())
-        fileToCipher.close()
-    #Convert the key to the right format
-    bytesKey = bytes.fromhex(key)
-    #Create Cipher function
-    cipher = AES.new(bytesKey, AES.MODE_ECB)
-    # Select first block of 16 bytes
-    block = newByteFile[0:AES.block_size]
-    #XOR block with iv
-    blockXORed = bytearray([_a ^ _b for _a, _b in zip(iv, block)])
-    #Cipher the result of the precedent XOR
-    blockciphered = bytearray(cipher.encrypt(blockXORed))
-    #Store the result in a variable
-    bytesCiphered = blockciphered
-
-    #Repeat the process to create CBC operation mode instead of ECB
-    for i in range(1,int(len(newByteFile)/AES.block_size)):
-        block = newByteFile[i*AES.block_size:(i+1)*AES.block_size]
-        blockXOR = bytearray([_a ^ _b for _a, _b in zip(blockciphered, block)])
-        blockcipheredNext = cipher.encrypt(blockXOR)
-        blockciphered = blockcipheredNext
-        bytesCiphered += blockciphered
-    
-    return [inputFile,blockciphered,iv]
-
-    
-def decipher(key,inputFile,iv):
-    #Generate IV
-    iv = Random.new().read(AES.block_size)
-    with open(inputFile, 'rb') as fileCiphered:
-        byteFileCiphered = bytearray(fileCiphered.read())
-        fileCiphered.close()
-    bytesKey = bytes.fromhex(key)
-    decipher = AES.new(bytesKey, AES.MODE_ECB)
-    #Ciphered Block
-    block = bytearray(byteFileCiphered[0:AES.block_size])
-    #Deciphered Before XOR
-    blockcipheredNext = bytearray(decipher.decrypt(block))
-    #Block Completely Deciphered
-    blockXOR = bytearray([_a ^ _b for _a, _b in zip(iv, blockcipheredNext)])
-    
-    bytesDeciphered = blockXOR
-
-    #Iterative to decipher the whole file
-    for i in range(1,int(len(byteFileCiphered)/AES.block_size)):
-        #Current Ciphered Block
-        currentBlock = byteFileCiphered[i*AES.block_size:(i+1)*AES.block_size]
-        #Decipher current block
-        blockDeciphered = decipher.decrypt(currentBlock)
-        #XOR result with old ciphered Block
-        blockXOR = bytearray([_a ^ _b for _a, _b in zip(blockDeciphered, block)])
-        block = currentBlock
-        #Add Deciphered Block to Bytearray
-        bytesDeciphered += blockXOR
-    
-    return [inputFile,bytesDeciphered]
+# Check if it is hexadecimal for the key
+def is_not_hex(s):
+    try:
+        int(s, 16)
+        return False
+    except ValueError:
+        return True
 
 
-
-#Create an array of lists, the last file is filled with name of input file, bytearray ciphered or decipher file, and iv
-def arrayFiles(key,inputfiles,encrypt):
-    #Encrypt all files in input
-    if encrypt:
-        for fileToCipher in inputfiles:
-            listofCiphered += cipherFile(key,fileToCipher)
-        return listofCiphered
-    #Decrypt all files in input thanks to the iv stored in a json file
+# Check if the file exist
+def file_exist(file_path):
+    if Path(file_path).is_file():
+        return True
     else:
-        #Open json of IV if it exist
-        if fileExist(getDirName(inputfiles)+"/iv.json"):
-            with open(getDirName(inputfiles)+"/iv.json",'rb') as jsonFile:
+        return False
+
+
+def get_file_name(absolute_path):
+    # Allow to get the filename without the absolute path
+    absolute_path = absolute_path[::-1]
+    file_name = ""
+    i = 0
+    while absolute_path[i] != "/":
+        file_name += absolute_path[i]
+        i += 1
+    return file_name[::-1]
+
+
+def get_dir_name(path):
+    path = path[::-1]
+    i = 0
+    while path[i] != "/":
+        i += 1
+    dir_name = path[i:]
+    return dir_name[::-1]
+
+
+def cipher_file(key, input_file):
+    # Generate IV
+    iv = bytearray(Random.new().read(AES.block_size))
+    # Open file in reading binary mode and store it inside a variable
+    print(iv)
+    print(iv)
+    print(iv)
+    print(iv)
+    print(key)
+    print(key)
+    print(key)
+    print(key)
+    with open(input_file, 'rb') as fileToCipher:
+        new_byte_file = bytearray(fileToCipher.read())
+        fileToCipher.close()
+    # Convert the key to the right format
+    bytes_key = bytes.fromhex(key)
+    # Create Cipher function
+    cipher = AES.new(bytes_key, AES.MODE_ECB)
+    # Select first block of 16 bytes
+    block = new_byte_file[0:AES.block_size]
+    # XOR block with iv
+    block_xored = bytearray([_a ^ _b for _a, _b in zip(iv, block)])
+    # Cipher the result of the precedent XOR
+    block_ciphered = bytearray(cipher.encrypt(block_xored))
+    # Store the result in a variable
+    bytes_ciphered = block_ciphered
+
+    # Repeat the process to create CBC operation mode instead of ECB
+    for i in range(1, int(len(new_byte_file) / AES.block_size)):
+        block = new_byte_file[i * AES.block_size:(i + 1) * AES.block_size]
+        block_xor = bytearray([_a ^ _b for _a, _b in zip(block_ciphered, block)])
+        block_ciphered_next = cipher.encrypt(block_xor)
+        block_ciphered = block_ciphered_next
+        bytes_ciphered += block_ciphered
+    return [input_file, block_ciphered, iv]
+
+
+def decipher_file(key, input_file, iv):
+    # Generate IV
+    print(iv)
+    print(iv)
+    print(iv)
+    print(iv)
+    print(key)
+    print(key)
+    print(key)
+    print(key)
+    with open(input_file, 'rb') as file_to_decipher:
+        byte_file_ciphered = bytearray(file_to_decipher.read())
+        file_to_decipher.close()
+    bytes_key = bytes.fromhex(key)
+    decipher = AES.new(bytes_key, AES.MODE_ECB)
+    # Ciphered Block
+    block = bytearray(byte_file_ciphered[0:AES.block_size])
+    # Deciphered Before XOR
+    block_ciphered_next = bytearray(decipher.decrypt(block))
+    # Block Completely Deciphered
+    block_xor = bytearray([_a ^ _b for _a, _b in zip(iv, block_ciphered_next)])
+
+    bytes_deciphered = block_xor
+
+    # Iterative to decipher the whole file
+    for i in range(1, int(len(byte_file_ciphered) / AES.block_size)):
+        # Current Ciphered Block
+        current_block = byte_file_ciphered[i * AES.block_size:(i + 1) * AES.block_size]
+        # Decipher current block
+        block_deciphered = decipher.decrypt(current_block)
+        # XOR result with old ciphered Block
+        block_xor = bytearray([_a ^ _b for _a, _b in zip(block_deciphered, block)])
+        block = current_block
+        # Add Deciphered Block to Byte_array
+        bytes_deciphered += block_xor
+
+    return [input_file, bytes_deciphered]
+
+
+# Create an array of lists, the last file is filled with name of input file, byte_array ciphered or decipher file, and iv
+def array_files(key, input_files, encrypt):
+    list_of_ciphered = []
+    # Encrypt all files in input
+    if encrypt:
+        for fileToCipher in input_files:
+            list_of_ciphered.append(cipher_file(key, fileToCipher))
+        print(list_of_ciphered)
+        return list_of_ciphered
+    # Decrypt all files in input thanks to the iv stored in a json file
+    else:
+        # Open json of IV if it exist
+        json_dict = {}
+        if file_exist(get_dir_name(input_files[0]) + "/iv.json"):
+            with open(get_dir_name(input_files[0]) + "/iv.json", 'r') as jsonFile:
                 json_dict = json.loads(jsonFile.read())
         else:
             print("The files to decrypt must be in the same directory as the iv.json file")
             exit()
-        for fileToDecipher in inputfiles:
-            listOfDeciphered += decipher(key,fileToCipher,json_dict[getFileName(fileToDecipher)])
-        return listOfDeciphered
-
-
+        list_of_deciphered = []
+        for fileToDecipher in input_files:
+            list_of_deciphered.append(decipher_file(key, fileToDecipher, bytearray(
+                [int(i) for i in json_dict[get_file_name(fileToDecipher[0:-4])].strip('][').split(', ')])))
+        return list_of_deciphered
 
 ############################################################
 ##
-#NEED TO TEST SUPPLY CHAIN BETWEEN CIPHER/DECIPHER ARRAYFILES and CREATEZIP#
+# NEED TO TEST SUPPLY CHAIN BETWEEN CIPHER/DECIPHER ARRAYFILES and CREATEZIP#
 ##
 ############################################################
--- Crypto_main.py
+++ Crypto_main.py
@@ -1,155 +1,137 @@
-import sys
-import re
-from pathlib import Path
+import json
 import os
 import shutil
-import json 
-import CryptoProject
-arguments = sys.argv
+import sys
+from pathlib import Path
+from CryptoProject import *
+
+inputs = sys.argv
 
 
-#Check if it is hexadecimal for the key
-def is_not_hex(s):
-    try:
-        int(s, 16)
-        return False
-    except ValueError:
-        return True
-
-#Check if the file exist
-def fileExist(filePath):
-    if Path(filePath).is_file():
-        print("exist")
-        return True
-    else:
-        print("NoExist")
-        return False
-  
-
-    
-def checkInputs(arguments):
-    sizeOfCommand = len(arguments)
-    #Check if the command as the minimum arguments needed
-    if sizeOfCommand < 8:
-        print("Please enter the right number of arguments.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+def check_inputs(arguments):
+    size_of_command = len(arguments)
+    # Check if the command as the minimum arguments needed
+    if size_of_command < 8:
+        print(
+            "Please enter the right number of arguments.\nThe Command as to be like this: filecrypt –enc|-dec –key "
+            "F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Verify if the first argument is -enc of -dec
+    # Verify if the first argument is -enc of -dec
     if arguments[1] != "-enc" and arguments[1] != "-dec":
         print(arguments[1])
-        print("You must choose if you want to encrypt or decrytp file(s).\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+        print(
+            "You must choose if you want to encrypt or decrytp file(s).\nThe Command as to be like this: filecrypt "
+            "–enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Verify the option -key is present
+    # Verify the option -key is present
     if arguments[2] != "-key":
-        print("You must enter the -key option followed by a password of 128 bits in hexadecimal.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+        print(
+            "You must enter the -key option followed by a password of 128 bits in hexadecimal.\nThe Command as to be "
+            "like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Check if the key is hexadecimal and his size is 128 bits
+    # Check if the key is hexadecimal and his size is 128 bits
     if len(arguments[3]) != 32 or is_not_hex(arguments[3]):
-        print("You must enter a key of 32 charater in hexadecimal.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+        print(
+            "You must enter a key of 32 charater in hexadecimal.\nThe Command as to be like this: filecrypt –enc|-dec "
+            "–key F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Check for the option -in
+    # Check for the option -in
     if arguments[4] != "-in":
-        print("You must enter the -in option to take files as input.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+        print(
+            "You must enter the -in option to take files as input.\nThe Command as to be like this: filecrypt "
+            "–enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Check for the option -out
-    if arguments[sizeOfCommand-2] != "-out":
-        print("You must enter the -out option to take files as output.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+    # Check for the option -out
+    if arguments[size_of_command - 2] != "-out":
+        print(
+            "You must enter the -out option to take files as output.\nThe Command as to be like this: filecrypt "
+            "–enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
         exit()
-    #Check if zipped file already exist and ask the user if he want to remove it
-    if fileExist(arguments[sizeOfCommand-1]+".zip"):
+    # Check if zipped file already exist and ask the user if he want to remove it
+    if file_exist(arguments[size_of_command - 1] + ".zip"):
         answer = ""
         while answer != "yes" and answer != "no":
             answer = input("The file you put as output file already exist do you want to overwrite it ?(yes/no)")
         if answer == "yes":
-            os.remove(arguments[sizeOfCommand-1]+".zip")
+            os.remove(arguments[- 1] + ".zip")
         else:
             print("If you don't want to delete the existing file please enter a different name")
             exit()
-    #We'll need the name to create a temporary directory so we check if it exist first
-    if os.path.isdir(arguments[sizeOfCommand-1]):
-            print("Please enter another name for the output file")
-    #Verify if the file(s) in input exists
-    for i in range(5,sizeOfCommand-2):
-        if not fileExist(arguments[i]):
-            print("The files you enter to cipher or decipher must exists.\nThe Command as to be like this: filecrypt –enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
-            print("There is at least one error for the "+str(i-4)+"st/nd/th file")
+    # We'll need the name to create a temporary directory so we check if it exist first
+    if os.path.isdir(arguments[- 1]):
+        print("Please enter another name for the output file")
+    # Verify if the file(s) in input exists
+    for i in range(5, size_of_command - 2):
+        if not file_exist(arguments[i]):
+            print(
+                "The files you enter to cipher or decipher must exists.\nThe Command as to be like this: filecrypt "
+                "–enc|-dec –key F...F(128bits) –in <input file(s)> -out <output file>")
+            print("There is at least one error for the " + str(i - 4) + "st/nd/th file")
             exit()
         else:
-            print("The file exist")
+            print("The file already exist")
     if arguments[1] == "-enc":
         return True
     else:
         return False
 
 
-def createZIP(zipfileName,files):
+def create_zip(zip_file_name, files):
     # Try to Create Directory to zip all the file together
     try:
-        os.mkdir(zipfileName, 0o755)
+        os.mkdir(zip_file_name, 0o755)
     except OSError:
-        print ("Creation of the directory %s failed" % zipfileName)
+        print("Creation of the directory %s failed" % zip_file_name)
         exit()
     else:
-        print ("Successfully created the directory %s" % zipfileName)
+        print("Successfully created the directory %s" % zip_file_name)
 
     if len(files[0]) == 2:
-        #Iterate in an array of type {[filePath1, bytesofcipheredFile1,iv],...}
-        for fileRessources in files:
-            #Crate file inside the newly created directory
-            with open(zipfileName+"/"+getFileName(fileRessources[0]), 'wb') as fileDeciphered:
-                fileDeciphered.write(fileRessources[1])
-                fileDeciphered.close()
+        # Iterate in an array of type {[filePath1, bytes of ciphered File1,iv],...}
+        for file_resources in files:
+            # Crate file inside the newly created directory
+            with open(zip_file_name + "/" + get_file_name(file_resources[0][0:-4]), 'wb') as file_deciphered:
+                file_deciphered.write(file_resources[1])
+                file_deciphered.close()
 
     elif len(files[0]) == 3:
-        dictForJSON = {}
-        if fileExist(getDirName(files[0][0])+"/iv.json"):
-            with open(getDirName(files[0][0])+"/iv.json",'rb') as jsonFile:
-                dictForJSON = jsonFile.loads(jsonFile.read())
+        dict_for_json = {}
+        if file_exist(get_dir_name(zip_file_name) + "/iv.json"):
+            with open(get_dir_name(zip_file_name) + "/iv.json", 'rb') as jsonFile:
+                dict_for_json = json.loads(jsonFile.read())
                 jsonFile.close()
 
-        for fileRessources in files:
-            #Create file inside the newly created directory
-            with open(zipfileName+"/"+getFileName(fileRessources[0]), 'wb') as fileCiphered:
-                fileCiphered.write(fileRessources[1])
+        for file_resources in files:
+            # Create file inside the newly created directory
+            with open(zip_file_name + "/" + get_file_name(file_resources[0]) + ".enc", 'wb') as fileCiphered:
+                fileCiphered.write(file_resources[1])
                 fileCiphered.close()
-            dictForJSON[getFileName(fileRessources[0])] = fileRessources[1]
 
-        #Write the new content of IVs in the json file
-        with open(zipfileName+"/iv.json", 'wb') as json:
-                json.write(json.dumps(dictForJSON))
-                json.close()
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            print(file_resources[2])
+            dict_for_json[get_file_name(file_resources[0])] = str(list(file_resources[2]))
+        # Write the new content of IVs in the json file
+        with open(zip_file_name + "/iv.json", 'w') as json_file:
+            json_file.write(json.dumps(dict_for_json))
+            json_file.close()
     # ZIP the directory
-    shutil.make_archive(zipfileName, 'zip', zipfileName)
-    #Remove the directory
-    shutil.rmtree(zipfileName)
+    shutil.make_archive(zip_file_name, 'zip', zip_file_name)
+    # Remove the directory
+    shutil.rmtree(zip_file_name)
 
-    
-
-def getFileName(absolutPath):
-    #Allow to get the filename without the absolute path
-    absolutPath = absolutPath[::-1]
-    fileName = ""
-    i = 0
-    while absolutPath[i] != "/":
-        fileName += absolutPath[i]
-        i += 1
-    return fileName[::-1]
-
-def getDirName(path):
-    path = path[::-1]
-    dirName = ""
-    i = 0
-    while path[i] != "/":
-        i += 1
-    dirName = path[i:]
-    return dirName[::-1]
 
 def main(arguments):
-    # checkInputs(arguments) UT needed for check if zip exist and check if directory exist
-    # createZIP() is working
-    getDirName("/Users/antoine/Dev/CryptoProject/CryptoProject/Crypto_main.py")
+    # check_inputs(arguments) UT needed for check if zip exist and check if directory exist
+    encrypt = check_inputs(arguments)
+    create_zip(arguments[-1], array_files(arguments[3], arguments[5: len(arguments) - 2], encrypt))
     pass
 
 
 if __name__ == "__main__":
-    main(arguments)
-
+    main(inputs)
